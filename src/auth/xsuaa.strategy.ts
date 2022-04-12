71068065fcd8604953c19bcf315345a8a8d9b205
Michał Majer <michal.majer@hotmail.com>
2022-04-12 08:44:19 +0200

    Update xsuaa.strategy.ts
    Hey,
    
    I spended some time to find out why it's not working.
    1) JWTStrategy from @sap/xssec should be used instead of passport-jwt, it's different implementation.
    2) super constructor need different parameters to be correctly initialized. I had to check strategy implementation from the SAP and I found that the second parameter is the forceType instead of a callback method.
    
    In my project I want also to resign from approuter. 
    Please update my changes, maybe it will help someone in the future :)

--- src/auth/xsuaa.strategy.ts
+++ src/auth/xsuaa.strategy.ts
@@ -1,26 +1,19 @@
-import { PassportStrategy } from '@nestjs/passport'
+import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
-import { Strategy, ExtractJwt } from 'passport-jwt';
-import * as xssec from '@sap/xssec';
+import { JWTStrategy } from '@sap/xssec';
+import { getServices } from '@sap/xsenv';
 import * as xsenv from '@sap/xsenv';
 
 @Injectable()
-export class XsuaaStrategy extends PassportStrategy(Strategy, 'xsuaa') {
-
+export class XsuaaStrategy extends PassportStrategy(JWTStrategy, 'JWT') {
+    
     constructor() {
-        // ?? Load environment from default-env.json
         xsenv.loadEnv();
-        // ?? Extract XSUAA configuration:
-        const uaaOptions = xsenv.getServices({ uaa: { tag: 'xsuaa' } }).uaa;
-        // !! Provide Passport with the authentication callback --> this will fail
-        super({
-            secretOrKeyProvider: new xssec.JWTStrategy(uaaOptions),
-            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
-        });
+        const xsuaa = getServices({ xsuaa: { tag: 'xsuaa' } }).xsuaa;
+        super(xsuaa, 'XSUAA');
     }
 
     validate(payload: any) {
         return payload;
     }
-
 }
